130ceb4d2c6739951ae5853e7ad26205492ac4b3
DoZTech <vincentadomale2@gmail.com>
2023-04-05 15:53:39 -0700

    fixing a red check

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Program that prints the number of arguments passed into it
- * @argc: - Int of arguments passed into program including command
- * @argv: - Array of pointers to the strings of arguments passed
- * Return: 0
- */
+* main - prints all arguments it received
+*@argc: argument count
+*@argv: argument vector
+*Return: returns 0
+*/
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i;
+		int i;
 
-	for (i = 0; argv[i] != '\0'; argc--, i++)
-		printf("%s\n", argv[i]);
+		for (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
 
-	return (0);
+		return (0);
 }
